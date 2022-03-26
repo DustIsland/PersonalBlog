320450c515bbcba086df0168459186c13f139e2e
DustIsland <664276861@qq.com>
2022-03-26 23:37:19 +0800

    third

--- config.js
+++ config.js
@@ -1,35 +1,13 @@
-var express = require("express");
-var globalConfig = require("./config");
-var loader = require("./loader");
+var fs = require("fs");
 
-var app = new express();
+var globalConfig = {};
 
-app.use(express.static("./page/"));
+var conf = fs.readFileSync("./server.conf");
 
-app.post("/editEveryDay", loader.get("/editEveryDay"));
-app.get("/queryEveryDay", loader.get("/queryEveryDay"));
+var configArr = conf.toString().split("\n");
 
-app.post("/editBlog", loader.get("/editBlog"));
-app.get("/queryBlogByPage", loader.get("/queryBlogByPage"));
+for (var i = 0 ; i < configArr.length ; i ++) {
+    globalConfig[configArr[i].split("=")[0].trim()] = configArr[i].split("=")[1].trim();
+}
 
-app.get("/queryBlogCount", loader.get("/queryBlogCount"));
-app.get("/queryBlogById", loader.get("/queryBlogById"));
-
-app.get("/addComment", loader.get("/addComment"));
-
-app.get("/queryRandomCode", loader.get("/queryRandomCode"));
-app.get("/queryCommentsByBlogId", loader.get("/queryCommentsByBlogId"));
-app.get("/queryCommentsCountByBlogId", loader.get("/queryCommentsCountByBlogId"));
-
-app.get("/queryAllBlog", loader.get("/queryAllBlog"));
-app.get("/queryRandomTags", loader.get("/queryRandomTags"));
-app.get("/queryHotBlog", loader.get("/queryHotBlog"));
-app.get("/queryNewComments", loader.get("/queryNewComments"));
-
-app.get("/queryByTag", loader.get("/queryByTag"));
-app.get("/queryByTagCount", loader.get("/queryByTagCount"));
-
-
-app.listen(globalConfig.port, function() {
-    console.log("服务器已启动");
-});
+module.exports = globalConfig;
